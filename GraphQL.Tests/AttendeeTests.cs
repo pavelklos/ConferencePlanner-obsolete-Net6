7d7013704cf1c0a2adb289b5c367f12f305ba5d5
Pavel Klos <pavelkloscz@gmail.com>
2023-06-17 10:35:41 +0200

    Add simple query tests

--- GraphQL.Tests/AttendeeTests.cs
+++ GraphQL.Tests/AttendeeTests.cs
@@ -9,7 +9,6 @@ using HotChocolate.Execution;
 using HotChocolate.Types;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Snapshooter.Xunit;
 
 namespace GraphQL.Tests
@@ -23,11 +22,15 @@ namespace GraphQL.Tests
             // act
             ISchema schema = await new ServiceCollection()
 
-                .AddPooledDbContextFactory<ApplicationDbContext>(
-                    (s, o) => o
-                        .UseSqlite("Data Source=conferences.db"))
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    (s, o) => o.UseInMemoryDatabase("Data Source=conferences.db"))
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddDbContextPool<ApplicationDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
 
-                .AddGraphQLServer()
+                .AddGraphQL()
+                //.AddGraphQLServer()
 
                 .AddQueryType()
                 .AddMutationType()
@@ -71,6 +74,166 @@ namespace GraphQL.Tests
 
             // assert
             schema.Print().MatchSnapshot();
+        }
+
+        [Fact]
+        public async Task Register_Attendee_By_Executor()
+        {
+            // arrange
+            IRequestExecutor executor = await new ServiceCollection()
+
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    (s, o) => o.UseInMemoryDatabase("Data Source=conferences.db"))
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddDbContextPool<ApplicationDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+
+                .AddGraphQL()
+                //.AddGraphQLServer()
+
+                .AddQueryType()
+                .AddMutationType()
+                .AddSubscriptionType()
+
+                .AddTypeExtension<AttendeeQueries>()
+                .AddTypeExtension<AttendeeMutations>()
+                .AddTypeExtension<AttendeeSubscriptions>()
+                .AddTypeExtension<AttendeeNode>()
+                .AddDataLoader<AttendeeByIdDataLoader>()
+
+                .AddTypeExtension<SessionQueries>()
+                .AddTypeExtension<SessionMutations>()
+                .AddTypeExtension<SessionSubscriptions>()
+                .AddTypeExtension<SessionNode>()
+                .AddDataLoader<SessionByIdDataLoader>()
+                .AddDataLoader<SessionBySpeakerIdDataLoader>()
+
+                .AddTypeExtension<SpeakerQueries>()
+                .AddTypeExtension<SpeakerMutations>()
+                .AddTypeExtension<SpeakerNode>()
+                .AddDataLoader<SpeakerByIdDataLoader>()
+                .AddDataLoader<SessionBySpeakerIdDataLoader>()
+
+                .AddTypeExtension<TrackQueries>()
+                .AddTypeExtension<TrackMutations>()
+                .AddTypeExtension<TrackNode>()
+                .AddDataLoader<TrackByIdDataLoader>()
+
+                .AddType<UploadType>()
+
+                // In this section we are adding extensions like relay helpers,
+                // filtering and sorting.
+                .AddFiltering()
+                .AddSorting()
+
+                // .EnableRelaySupport()
+                .AddGlobalObjectIdentification()
+
+                .BuildRequestExecutorAsync();
+
+            // act
+            IExecutionResult result = await executor.ExecuteAsync(@"
+                mutation Register_Attendee {
+                    registerAttendee(
+                    input: {
+                        emailAddress: ""michael@chillicream.com""
+                        firstName: ""michael""
+                        lastName: ""staib""
+                        userName: ""michael1""
+                    }
+                    ) {
+                    attendee {
+                        id
+                    }
+                    }
+                }
+            ");
+
+            // assert
+            result.ToJson().MatchSnapshot();
+        }
+
+        [Fact]
+        public async Task Register_Attendee_By_Services()
+        {
+            // arrange
+            IServiceProvider services = new ServiceCollection()
+
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    (s, o) => o.UseInMemoryDatabase("Data Source=conferences.db"))
+                //.AddPooledDbContextFactory<ApplicationDbContext>(
+                //    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                .AddDbContextPool<ApplicationDbContext>(
+                    options => options.UseInMemoryDatabase("Data Source=conferences.db"))
+                
+                .AddGraphQL()
+                //.AddGraphQLServer()
+
+                .AddQueryType()
+                .AddMutationType()
+                .AddSubscriptionType()
+
+                .AddTypeExtension<AttendeeQueries>()
+                .AddTypeExtension<AttendeeMutations>()
+                .AddTypeExtension<AttendeeSubscriptions>()
+                .AddTypeExtension<AttendeeNode>()
+                .AddDataLoader<AttendeeByIdDataLoader>()
+
+                .AddTypeExtension<SessionQueries>()
+                .AddTypeExtension<SessionMutations>()
+                .AddTypeExtension<SessionSubscriptions>()
+                .AddTypeExtension<SessionNode>()
+                .AddDataLoader<SessionByIdDataLoader>()
+                .AddDataLoader<SessionBySpeakerIdDataLoader>()
+
+                .AddTypeExtension<SpeakerQueries>()
+                .AddTypeExtension<SpeakerMutations>()
+                .AddTypeExtension<SpeakerNode>()
+                .AddDataLoader<SpeakerByIdDataLoader>()
+                .AddDataLoader<SessionBySpeakerIdDataLoader>()
+
+                .AddTypeExtension<TrackQueries>()
+                .AddTypeExtension<TrackMutations>()
+                .AddTypeExtension<TrackNode>()
+                .AddDataLoader<TrackByIdDataLoader>()
+
+                .AddType<UploadType>()
+
+                // In this section we are adding extensions like relay helpers,
+                // filtering and sorting.
+                .AddFiltering()
+                .AddSorting()
+
+                // .EnableRelaySupport()
+                //.AddGlobalObjectIdentification()
+
+                .Services
+                .BuildServiceProvider();
+
+            // act
+            IExecutionResult result = await services.ExecuteRequestAsync(
+                QueryRequestBuilder.New()
+                    .SetQuery(@"
+                        mutation Register_Attendee {
+                            registerAttendee(
+                            input: {
+                                emailAddress: ""michael@chillicream.com""
+                                firstName: ""michael""
+                                lastName: ""staib""
+                                userName: ""michael1""
+                            }
+                            ) {
+                            attendee {
+                                id
+                            }
+                            }
+                        }
+                    ")
+                    .Create());
+
+            // assert
+            result.ToJson().MatchSnapshot();
         }
 
         [Fact]
